4436d99a684d31609c5b2602df282d6d626ff2c1
Tiago <tiago.americano.03@gmail.com>
2021-02-27 18:05:02 -0300

    Adicionei tela de login

--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,27 @@ import axios from "axios"
 import {useState} from "react"
 import Styles from '../styles/index'
 import Vis from '../Components'
+import ReactLoading from 'react-loading';
+ 
 function Home(){
   const [value, setValue] = useState('')
-  const [response, setResponse] = useState('')
-  const [teste, setTeste] = useState(0)
-  const [visibleTypes, setVisibleTypes] = useState(["unvisible", "unvisible", "unvisible", "unvisible"])
-  const [types, setStypes] =  useState(["Significado", "Sinônimos", "Expressão", "Citação"])
-  const [texts, setTexts] =  useState(["", "", "", ""])
-  const [displayTypes, setDisplayTypes] =  useState(["unvisible", "unvisible", "unvisible", "unvisible"])
-  const [words, setWords] = useState({nodes: [], edges: []})
+
+  const [words, setWords] = useState({nodes: [], edges: [], status: null})
 
   function handleSubmit(event){
     event.preventDefault()
+    setWords({nodes: [], edges: [], status:'loading'})
     axios.post("/api/getRelations", {value}).then(  res=>{
    
-     console.log(res.data.value)
-      setWords(res.data.value)
+     const {nodes, edges} = res.data.value
+     const Words = {nodes, edges, status: "run"}
+      setWords(Words)
     })
   }
   function handleInput(event){
     setValue(event.target.value)
   }
-  function handleRelationType(type){
-    const VisibleTypes = visibleTypes
-    VisibleTypes[types.indexOf(type)] === "unvisible" ? VisibleTypes[types.indexOf(type)] = "visible" : VisibleTypes[types.indexOf(type)] = "unvisible"
-    setVisibleTypes(VisibleTypes)
-    setTeste(teste+1)
-    console.log(VisibleTypes)
-    // setVisibleTypes(["teste"])
-  }
+
   return(
     <Styles>
       <form onSubmit={handleSubmit}>
@@ -38,17 +30,15 @@ function Home(){
 
 
       </form>
-      <div className="tiposRelações">
       
       
-  <div className="unvisible">{teste}</div>
-      </div>
+
       <style jsx global>{`
       body{
         background-color: #1E90FF;
       }
       `}</style>
-      <Vis Nodes={words.nodes} Edges={words.edges}/>
+      {!words.status?null:(words.status==='loading'?<ReactLoading  />:<Vis Nodes={words.nodes} Edges={words.edges}/>)}
     </Styles>
   )
 }
